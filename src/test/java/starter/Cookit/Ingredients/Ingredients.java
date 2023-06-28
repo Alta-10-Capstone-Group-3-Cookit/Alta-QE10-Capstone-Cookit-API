236c93c499a58dcfa7b52b0da6a74467640d0a91
Khairun Najmi Mukaffa <knmukafagmail.comgit config --global user.email knmukafagmail.com>
2023-06-28 15:36:29 +0700

    Develop I_Ingredients

---
+++ src/test/java/starter/Cookit/Ingredients/Ingredients.java
@@ -0,0 +1,35 @@
+package starter.Cookit.Ingredients;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+import starter.Utils.Constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ingredients {
+    @Step("Post Insert New Recipe's Ingredient")
+    public void postInsertNewRecipesIngredient(String name, int price) {
+        Map<String, Object> ingredient = new HashMap<>();
+        ingredient.put("name", name);
+        ingredient.put("price", price);
+
+        SerenityRest.given()
+                .header("Authorization", "Bearer " + Constants.BEARER_TOKEN)
+                .contentType(ContentType.JSON)
+                .body(ingredient);
+    }
+
+    @Step("Put Update Recipe's Ingredient")
+    public void putUpdateRecipesIngredient(String name, int price) {
+        Map<String, Object> ingredient = new HashMap<>();
+        ingredient.put("name", name);
+        ingredient.put("price", price);
+
+        SerenityRest.given()
+                .header("Authorization", "Bearer " + Constants.BEARER_TOKEN)
+                .contentType(ContentType.JSON)
+                .body(ingredient);
+    }
+}
---
+++ src/test/resources/features/I_Ingredients/B_PutUpdateRecipe'sIngredient.feature
@@ -0,0 +1,8 @@
+Feature: Put update recipe's ingredients
+
+
+  Scenario: Put update recipe's ingredients with valid recipe id and ingredient id and json body
+    Given Put update recipe's ingredients with valid recipe id and ingredient id and json body
+    When Send Put update recipe's ingredients
+    Then Status code should be 200 OK
+    And Validate put update recipe's ingredients with valid recipe id and ingredient id and json body json schema
